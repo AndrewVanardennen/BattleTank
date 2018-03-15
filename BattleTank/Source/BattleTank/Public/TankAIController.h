de2b9f217b70ad6648de13b3912e54bec62f1027
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2018-03-15 20:27:47 +0100

    Slowdown Firing rate :rocket:

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -15,17 +15,7 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
-	virtual void BeginPlay() override;
-
 private:
-
-	
-	ATank* GetControlledTank() const;
-
-	ATank* GetPlayerTank() const;
-	
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime);
-	
-	
-	
 };
