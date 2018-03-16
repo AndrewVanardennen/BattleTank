869395504e88f26d4895685a8a8c39ce80340857
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2018-03-16 22:12:07 +0100

    Try input Axis

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to set maximum driving force and to appy force to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public: 
+	UFUNCTION(BlueprintCallable, Category = input)
+	void SetThrottle(float Throttle);
+	
+	// TODO clamp actual throttle value so player can't over-drive
+	
+	
+};
