e7ffed28b0c35e360776d58dc82c15217565f289
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2018-03-17 06:53:17 +0100

    Rework MovementComponents

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,7 +6,7 @@
 void UTankTrack::SetThrottle(float Throttle)
 {
 
-	UE_LOG(LogTemp, Warning, TEXT("throttle: %f"), Throttle);
+	
 	// TODO clamp actual throttle value so player can't over-drive
 
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
